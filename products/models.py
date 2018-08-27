04ec38a6ad2081657faded531bc41c7a229140b8
Gilly <gillianohea@gmail.com>
2018-08-27 17:31:08 +0000

    update models

--- products/models.py
+++ products/models.py
@@ -9,18 +9,21 @@ from django.db import models
     
     # def __str__(self):
     #     return self.name
-
+    
+    
+class PostPicture(models.Model):
+    picture = models.ImageField(upload_to='images')
+    productid = models.ForeignKey('Product')
+    
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    userid = models.ForeignKey(admin, related_name='userblogpost')
+    # userid = models.ForeignKey(admin, related_name='userblogpost')
     Picture = None
 
 
-class Picture(models.Model):
-    picture = models.ImageField(upload_to='images')
-    productid = models.ForeignKey('Product')
+
     
     
     def __unicode__(self):
