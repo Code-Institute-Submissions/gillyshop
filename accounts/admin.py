ef659df6262ca4b99dc54c4df1880bd0b463fe0b
Gilly <gillianohea@gmail.com>
2018-08-27 17:46:57 +0000

    update models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from products.models import Product, PostPicture
+# from products.models import Product, PostPicture
 
 # Register your models here.
-class PostPictureInline(admin.TabularInline):
-    model = PostPicture
-    fields = ['picture',]
+# class PostPictureInline(admin.TabularInline):
+#     model = PostPicture
+#     fields = ['picture',]
     
-class ProductAdmin(admin.ModelAdmin):
-    inlines = [PostPictureInline,]
+# class ProductAdmin(admin.ModelAdmin):
+#     inlines = [PostPictureInline,]
     
 # admin.site.register(ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 
-# Create your models here.
-# class Product(models.Model):
-#     name = models.CharField(max_length=254, default='')
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = models.ImageField(upload_to='images')
-    
-    # def __str__(self):
-    #     return self.name
-    
-    
-class PostPicture(models.Model):
-    image = models.ImageField(upload_to='images')
-    productid = models.ForeignKey('Product')
-    
+Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    # userid = models.ForeignKey(admin, related_name='userblogpost')
-    # Picture = None
-
-
-
-    
+    image = models.ImageField(upload_to='images')
     
     def __str__(self):
         return self.name
+    
+    
+# class PostPicture(models.Model):
+#     image = models.ImageField(upload_to='images')
+#     productid = models.ForeignKey('Product')
+    
+# class Product(models.Model):
+#     name = models.CharField(max_length=254, default='')
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     # userid = models.ForeignKey(admin, related_name='userblogpost')
+#     # Picture = None
+
+
+
+    
+    
+#     def __str__(self):
+#         return self.name
         
 
         
