61c8c9c44d7eb904a06677e9d2bf2bb658dafa49
Gilly <gillianohea@gmail.com>
2018-08-27 17:15:51 +0000

    attempt to upload mult files

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from products.models import Product, Picture
 
 # Register your models here.
+class PictureInline(admin.TabularInline):
+    model = Picture
+    fields = ['picture',]
+    
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PictureInline,]
+    
+admin.site.register(Product, ProductAdmin)
