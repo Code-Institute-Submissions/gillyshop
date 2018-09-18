c5fc2374adf8302be95f0b4475312aebcc5b54c0
Gilly <gillianohea@gmail.com>
2018-09-18 19:07:44 +0000

    add tags to admin products

--- products/models.py
+++ products/models.py
@@ -5,11 +5,12 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    tags = models.CharField(max_length=254, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
-        return self.name
+        return self.namegit 
     
     
 # class PostPicture(models.Model):
