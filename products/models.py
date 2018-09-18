a2fc509e404f354fc713c7c423aef1bb7f5d6656
Gilly <gillianohea@gmail.com>
2018-09-18 19:15:31 +0000

    remove tags

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    tags = models.CharField(max_length=254, default='')
+    # tags = models.CharField(max_length=254, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
