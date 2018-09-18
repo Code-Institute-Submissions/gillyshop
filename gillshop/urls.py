0fc5cfc68395293676a495fa75c83fb2c6debbec
Gilly <gillianohea@gmail.com>
2018-09-18 12:34:23 +0000

    update index urls

--- gillshop/urls.py
+++ gillshop/urls.py
@@ -20,15 +20,17 @@ from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
 from checkout import urls as urls_checkout
-from home import urls as urls_home
-from products.views import all_products
+# added home url
+from home.views import index
+# from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include(urls_home)), 
-    url(r'^$', all_products, name='index'),
+    # added home url
+    url(r'^$', index, name='index'),
+    # url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
