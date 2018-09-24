b99264c1de5def09915e2c73c130b1ea32cce79a
Gilly <gillianohea@gmail.com>
2018-09-24 19:04:02 +0000

    add modeadmin class

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Product
 
+
+class ProdModelAdmin(admin.ModelAdmin):
+    list_display = ['tag_list']
+    
+    def get_queryset(self, request):
+        return super(ProdModelAdmin, self).get_queryset(request).prefetch_related('tags')
+        
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+        
+
+
 # Register your models here.
 admin.site.register(Product)
