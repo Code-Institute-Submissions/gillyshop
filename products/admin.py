d8b00fe0fc9232f76bc48427df9f6d10b127603f
Gilly <gillianohea@gmail.com>
2018-09-24 19:12:37 +0000

    add objets to model

--- products/admin.py
+++ products/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from .models import Product
 
 
-class ProdModelAdmin(admin.ModelAdmin):
-    list_display = ['tag_list']
+# class ProdModelAdmin(admin.ModelAdmin):
+#     list_display = ['tag_list']
     
-    def get_queryset(self, request):
-        return super(ProdModelAdmin, self).get_queryset(request).prefetch_related('tags')
+#     def get_queryset(self, request):
+#         return super(ProdModelAdmin, self).get_queryset(request).prefetch_related('tags')
         
-    def tag_list(self, obj):
-        return u", ".join(o.name for o in obj.tags.all())
+#     def tag_list(self, obj):
+#         return u", ".join(o.name for o in obj.tags.all())
         
 
 
--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from taggit.managers import TaggableManager
 
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
+    objects = models.Manager
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
