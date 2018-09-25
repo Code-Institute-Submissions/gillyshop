62a86af7af6f873f809d6e2de77ad5b4ebbce904
Gilly <gillianohea@gmail.com>
2018-09-25 12:53:36 +0000

    update new

--- search/views.py
+++ search/views.py
@@ -6,6 +6,5 @@ from products.models import Product
 
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    products = Product.objects.filter(tags__icontains=request.GET['q'])
     return render(request, "products.html", {"products":products})
     
