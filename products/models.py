217cd06ec891d20224c03d1b1f1f6f99e1dfa156
Gilly <gillianohea@gmail.com>
2018-09-26 12:39:20 +0000

    add new urlfield class

--- products/models.py
+++ products/models.py
@@ -8,8 +8,12 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    url = models.URLField()
     tags = TaggableManager()
+    
+class URLField(models.Model):
+    url = models.CharField(max_length=150)
+    
+    
     
     def __str__(self):
         return self.name
