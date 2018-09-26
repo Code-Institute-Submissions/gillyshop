b3d3f3fa8c4fe4c6a11aeb500052a1c4275975e8
Gilly <gillianohea@gmail.com>
2018-09-26 19:44:39 +0000

    add cactus url

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products, new
+from .views import all_products, new, cactus
 
 urlpatterns = [
     url(r'^$', all_products, name="products"),
     url(r'^new/$', new, name="new"),
+    url(r'^cactus/$', cactus, name="cactus"),
     ]
