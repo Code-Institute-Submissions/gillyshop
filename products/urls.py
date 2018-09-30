1148ba1b2dc90875184ca247d3b9b60333ac9502
Gilly <gillianohea@gmail.com>
2018-09-30 15:19:36 +0000

    add new views and pages

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_products, new, cactus
+from .views import all_products, new, cactus, animals, flamingos, unicorns
 
 urlpatterns = [
     url(r'^$', all_products, name="products"),
     url(r'^new/$', new, name="new"),
     url(r'^cactus/$', cactus, name="cactus"),
+    url(r'^animals/$', animals, name="animals"),
+    url(r'^flamingos/$', flamingos, name="flamingos"),
+    url(r'^unicorns/$', unicorns, name="unicorns"),
     ]
--- products/views.py
+++ products/views.py
@@ -13,3 +13,15 @@ def new(request):
 def cactus(request):
     cactusproducts = Product.objects.filter(category="Cactus")
     return render(request, "cactus.html", {"products": cactusproducts})
+    
+def animals(request):
+    animalproducts = Product.objects.filter(category="Animal")
+    return render(request, "animals.html", {"products": animalproducts})
+    
+def flamingos(request):
+    flamingoproducts = Product.objects.filter(category="Flamingo")
+    return render(request, "flamingo.html", {"products": flamingoproducts})
+    
+def unicorns(request):
+    unicornproducts = Product.objects.filter(category="Unicorns")
+    return render(request, "unicorns.html", {"products": unicornproducts})
