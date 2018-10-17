afef0f724e18e36a2cb042441d6bb7a72e4dd6c2
Gilly <gillianohea@gmail.com>
2018-10-17 12:34:01 +0000

    migrated and updated model

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    # image = models.ImageField(upload_to='images')
     tags = TaggableManager()
     url = models.TextField(max_length=200, default='')
     category = models.CharField(max_length=100, default='')
@@ -42,3 +42,6 @@ class Product(models.Model):
         
 
         
+class Image(models.Model):
+    image = models.ImageField(upload_to='images')
+    productimg = models.ForeignKey('Product', related_name='images')
