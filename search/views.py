48cdd3a274e584d6238d1f328154aac2042225fd
Gilly <gillianohea@gmail.com>
2018-10-21 15:27:55 +0000

    test search

--- search/views.py
+++ search/views.py
@@ -6,6 +6,7 @@ from products.models import Product
 
 def do_search(request):
     # products = Product.objects.filter(name__icontains=request.GET['q'])
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(name__icontains=request.GET['q']) | Product.objects.filter(description__icontains=request.GET['q'])
     return render(request, "products.html", {"products":products})
+    
     
