c4e6d776e658e82a07cb857df019e9c0f9fc8b31
Gilly <gillianohea@gmail.com>
2018-10-24 12:30:28 +0000

    update form fields remove phone no.

--- checkout/forms.py
+++ checkout/forms.py
@@ -16,4 +16,4 @@ class MakePaymentForm(forms.Form):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county' )
+        fields = ('full_name', 'address_line1', 'address_line2', 'town_or_city','county', 'country', 'postcode' )
