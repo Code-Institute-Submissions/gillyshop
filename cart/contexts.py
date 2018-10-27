523d27c8539cd0273a3a1d5df2a34f229d8630bd
Gilly <gillianohea@gmail.com>
2018-10-27 15:24:19 +0000

    update cart to reflect product totals

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,15 +12,19 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    itemtotal = 0
+    item_total = 0
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
-        itemtotal += product.quantity * product.price
+        item_total += product.id * quantity * product.price
         product_count += quantity
         cart_items.append({'id':id, 'quantity':quantity, 'product': product})
         
         
-    return { 'cart_items': cart_items, 'total': total, 'product_count': product_count }
+    return { 'cart_items': cart_items, 'total': total, 'product_count': product_count, 'item_total': item_total }
+    
+    
+    
+    
     
         
