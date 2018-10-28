5b4232ceb78948eb389e3c6b8e012efff1c14283
Gilly <gillianohea@gmail.com>
2018-10-28 12:48:39 +0000

    update tag view

--- products/views.py
+++ products/views.py
@@ -54,7 +54,7 @@ class TagIndexView(ListView):
         return Product.objects.filter(tags__slug=self.kwargs.get('slug'))
 
 def tag(request):
-    tag = Product.objects.filter(slug="slug")
+    tag = Product.objects.all()
     return render(request, "tag.html", {"tag":tag})
 
 
