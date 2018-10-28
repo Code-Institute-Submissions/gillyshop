904aceea54d515c38ff38832c9cdf5684b7fea0d
Gilly <gillianohea@gmail.com>
2018-10-28 11:19:05 +0000

    update tag url

--- products/urls.py
+++ products/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^deer/$', deer, name="deer"),
     url(r'^galaxy/$', galaxy, name="galaxy"),
     url(r'^(?P<pk>\d+)/$', product_detail, name="product_detail"),
-    url(r'^tag/(?P<slug>[-\w]+)/$', TagIndexView.as_view(), name='tagged'),
+    url(r'^tag/(?P<slug>[-\w]+)/$', TagIndexView.as_view(), name='tag'),
     ]
     
     
