79e4e878d6c3884d34bfa0cc73cb9fdd76eefe7c
Gilly <gillianohea@gmail.com>
2018-11-01 19:36:29 +0000

    return add to cart to all product pages

--- accounts/views.py
+++ accounts/views.py
@@ -33,7 +33,6 @@ def login(request):
             
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in")
                 return redirect(reverse('profile'))
             else: 
                 login_form.add_error(None, "Your username or password is incorrect")
