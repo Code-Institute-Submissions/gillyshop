e5e21eae7c3a95a57a2417e26f4cf96cef29d850
Gilly <gillianohea@gmail.com>
2018-11-05 18:05:05 +0000

    update

--- gillshop/settings.py
+++ gillshop/settings.py
@@ -176,8 +176,8 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = 'gillianoheaphotos7@gmail.com'
+EMAIL_HOST_PASSWORD = 'Dj@ngogill'
 EMAIL_PORT = 587
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
