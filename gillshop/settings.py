c6b07b6b251596879c32ee2e5d7bd907c5115139
Gilly <gillianohea@gmail.com>
2018-11-05 17:51:13 +0000

    update email settings

--- gillshop/settings.py
+++ gillshop/settings.py
@@ -180,9 +180,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
-
-
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 
