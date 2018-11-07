92e87bac1ed6ce8bed36d9a86494f0fc47b05586
Gilly <gillianohea@gmail.com>
2018-11-07 15:15:15 +0000

    update checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -53,9 +53,10 @@ def checkout(request):
                 messages.error(request, "Your card was declined")
                 
             if customer.paid:
-                # messages.error(request, "You have successfully paid")
-                # request.session['cart'] = {}
-                return redirect(reverse('paid'))
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                # return redirect(reverse('paid'))
+                return render(request, 'paid.html')
                 
             else: 
                 messages.error(request, "Unable to take payment")
