3322b05a1ae226a6013563b490c7531982dbec40
Gilly <gillianohea@gmail.com>
2018-11-14 19:37:15 +0000

    update checkout view and urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import checkout
+from .views import checkout, paid
 
 
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),
-    # url(r'^$', paid, name='paid')
+    url(r'^$', paid, name='paid')
     ]
