eb6a55f8c4a0fc0619c7d1461f92a2ee699da993
Gilly <gillianohea@gmail.com>
2018-11-20 13:04:24 +0000

    change name in order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from products.models import Product
 # Create your models here.
 
 class Order(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=50, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
