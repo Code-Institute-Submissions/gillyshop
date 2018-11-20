3fd5a8b2ca773044a8dce06a7dba7fc28b47349b
Gilly <gillianohea@gmail.com>
2018-11-20 13:44:37 +0000

    update year choices

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,7 +4,7 @@ from .models import Order
 class MakePaymentForm(forms.Form):
     
     MONTH_CHOICES = [(i,i) for i in range (1, 13)]
-    YEAR_CHOICES = [(i, i) for i in range (2017, 2036)]
+    YEAR_CHOICES = [(i, i) for i in range (2018, 2036)]
     
     credit_card_number = forms.CharField(label='Credit Card Number', required=False, widget=forms.TextInput(attrs={'placeholder': '**** **** **** ****'}))
     csc = forms.CharField(label='Security code (CVV)', required=False, widget=forms.TextInput(attrs={'placeholder': '***'}))
