0c287dda33662d78ed767dcc16c9b74be8c7c810
Gilly <gillianohea@gmail.com>
2018-11-27 14:03:58 +0000

    rest reutrn if no search items found

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ def do_search(request):
     # products = Product.objects.filter(name__icontains=request.GET['q'])
     products = Product.objects.filter(name__icontains=request.GET['q']) | Product.objects.filter(description__icontains=request.GET['q'])
     return render(request, "products.html", {"products":products})
-    if not products:
-     return render(request, "shipping.html")
+    else ObjectDoesNotExist:
+        print("Either the entry or blog doesn't exist.")
     
     
