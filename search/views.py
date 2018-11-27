6c0780c3214ae1efa9011fecb1c37165b4e27cfc
Gilly <gillianohea@gmail.com>
2018-11-27 16:55:50 +0000

    update search view

--- search/views.py
+++ search/views.py
@@ -6,9 +6,13 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def do_search(request):
+    if request.method == 'get':
     # products = Product.objects.filter(name__icontains=request.GET['q'])
-    products = Product.objects.filter(name__icontains=request.GET['q']) | Product.objects.filter(description__icontains=request.GET['q'])
-    return render(request, "products.html", {"products":products})
+        products = Product.objects.filter(name__icontains=request.GET['q']) | Product.objects.filter(description__icontains=request.GET['q'])
+        return render(request, "products.html", {"products":products})
+        
+    else:
+        return render(request, "shipping.html")
 
     
     
