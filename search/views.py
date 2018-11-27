9635cb2db84ce3d9cb0d47cf175c2db64e62770b
Gilly <gillianohea@gmail.com>
2018-11-27 14:05:52 +0000

    remove test

--- search/views.py
+++ search/views.py
@@ -9,7 +9,6 @@ def do_search(request):
     # products = Product.objects.filter(name__icontains=request.GET['q'])
     products = Product.objects.filter(name__icontains=request.GET['q']) | Product.objects.filter(description__icontains=request.GET['q'])
     return render(request, "products.html", {"products":products})
-    else ObjectDoesNotExist:
-        print("Either the entry or blog doesn't exist.")
+
     
     
