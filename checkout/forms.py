346567924905ae10e2be3ad9dedb3ca02786a44d
Gilly <gillianohea@gmail.com>
2018-12-02 11:26:51 +0000

    amend checkout view

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,7 +9,7 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range (2018, 2036)]
     
     credit_card_number = forms.CharField(label='Credit Card Number', required=False, widget=forms.TextInput(attrs={'placeholder': '**** **** **** ****', 'class':'ccclass'}))
-    cvv = forms.CharField(label='Security code (CVV)', required=False, widget=forms.TextInput(attrs={'placeholder': '***'}))
+    cvv = forms.CharField(label='Security code (CVV)', required=True, widget=forms.TextInput(attrs={'placeholder': '***'}))
     expiry_month = forms.ChoiceField(label='Expiry Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Expiry Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
