649504c3eacd85b58b1e3ecfc028f247a7c6caba
Gilly <gillianohea@gmail.com>
2018-12-02 11:37:13 +0000

    amend checkout vie and readme

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,10 +8,10 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i,i) for i in range (1, 13)]
     YEAR_CHOICES = [(i, i) for i in range (2018, 2036)]
     
-    credit_card_number = forms.CharField(label='Credit Card Number', required=True, widget=forms.TextInput(attrs={'placeholder': '**** **** **** ****', 'class':'ccclass'}))
-    cvv = forms.CharField(label='Security code (CVV)', required=True, widget=forms.TextInput(attrs={'placeholder': '***'}))
-    expiry_month = forms.ChoiceField(label='Expiry Month', choices=MONTH_CHOICES, required=True)
-    expiry_year = forms.ChoiceField(label='Expiry Year', choices=YEAR_CHOICES, required=True)
+    credit_card_number = forms.CharField(label='Credit Card Number', required=False, widget=forms.TextInput(attrs={'placeholder': '**** **** **** ****', 'class':'ccclass'}))
+    cvv = forms.CharField(label='Security code (CVV)', required=False, widget=forms.TextInput(attrs={'placeholder': '***'}))
+    expiry_month = forms.ChoiceField(label='Expiry Month', choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label='Expiry Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
     
     
